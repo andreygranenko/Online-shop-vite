763faebcceab4875b1b302da8589f9ca6fbc77c7
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-04-15 23:31:52 +0300

    configured supabase and added a column

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import MainPage from "./components/pages/main-page/MainPage.jsx";
-
+import {useState} from "react";
+import {supabase} from "./client.js";
 
 function App() {
 
-
+  const [products, setProducts] = useState([])
+  async function fetchProducts() {
+    const {data} = await supabase
+      .from('products')
+      .select()
+      .setProducts(data)
+  }
 
   return (
     <>
--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
+import {key, url} from "./key.js";
 
 export const supabase = createClient(
-
+  url,
+  key
 )
