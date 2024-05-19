fa9e901a08437f9d9363074ab315575609f5cecd
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-05-19 22:41:21 +0300

    updated faq page

--- src/components/pages/faq-page/faqPage.jsx
+++ src/components/pages/faq-page/faqPage.jsx
@@ -1,15 +1,25 @@
 import {motion} from "framer-motion";
-
+import './faq-page.sass';
 const FaqPage = () => {
   return (
     <>
       <motion.div
+        className={'container faq-page '}
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         exit={{opacity: 0}}
 
       >
-        <h2>FAQ Page</h2>
+        <img src="https://i.ibb.co/NsWWJkf/Frame-26086945.png" alt="faq"/>
+        <aside>
+          Table of Contents
+          <ul>
+            <li><a href="#">General</a></li>
+            <li><a href="#">Trusts & Safety</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Billing</a></li>
+          </ul>
+        </aside>
       </motion.div>
     </>
 
