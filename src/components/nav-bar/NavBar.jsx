d90d2fcf31bded8f7da9d2dbcecc38ac37b0f951
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-05-20 20:34:53 +0300

    updated navbar

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -6,32 +6,45 @@ import {Link} from "react-router-dom";
 const NavBar = () => {
   return (
     <>
-      <nav>
-        <div className="container">
-          <div className="row ">
-            <div className="nav_logo col-md-1">
-              <Link to={'/'}><img src={Logo} alt="logo" className="logo"/></Link>
-            </div>
-            <ul className="menu col-md-6 offset-md-1">
-              <li className=" menu_item"><Link to={'/'}>Home</Link> </li>
-              <li className=" menu_item"><Link to={'/produkti'}>Products</Link> </li>
-              <li className=" menu_item"><Link to={'/blogs'}>Blog</Link></li>
-              <li className=" menu_item"><Link to={'/faq'}>FAQ</Link></li>
-              <li className=" menu_item"><Link to={'/kontakti'}>Contact Us</Link></li>
-            </ul>
-            <div className="action_btns col-md-3 offset-md-1">
-              <a href="#"><img id={'search'} src={Search} className="action_img" alt="loop"/></a>
-              <Link to={'/account'}><img src={Basket} className="action_img" alt="basket"/></Link>
-              <Link to={'/login'}>
-                <button>
-                  Log in / Sign Up
-                </button>
-              </Link>
 
+      <div className="px-10 navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a>Item 1</a></li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </li>
+              <li><a>Item 3</a></li>
+            </ul>
           </div>
+          <Link to={'/'} className="btn btn-ghost text-xl">Vaite</Link>
         </div>
-      </nav>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to={'/produkti'}>Products</Link></li>
+            <li>
+              <Link to={'/blogs'}>Blog</Link>
+            </li>
+            <li><Link to={'/faq'}>FAQ</Link></li>
+            <li><Link to={'/kontakti'}>Contact Us</Link></li>
+
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="form-control px-4">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          </div>
+          <Link to={'/login'} className="btn bg-primary text-primary-content">Log in / Sign Up</Link>
+        </div>
+      </div>
       <hr/>
     </>
   )
