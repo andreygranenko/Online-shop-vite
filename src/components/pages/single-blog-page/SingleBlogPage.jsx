b822985766a3788d5bada06eea63666bb3eeefab
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-06-22 23:33:40 +0300

    updated single blog page

--- src/components/pages/single-blog-page/SingleBlogPage.jsx
+++ src/components/pages/single-blog-page/SingleBlogPage.jsx
@@ -29,11 +29,23 @@ const SingleBlogPage = () => {
   return (
     <div>
       {blogItem && (
-        <div className="container mt-5">
-          <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold text-center">{blogItem.title}</h2>
-            <img className="w-2/5" src={blogItem.img_url} alt={blogItem.title} />
+        <div className="container mt-5 flex gap-10">
+          <div className="flex flex-col  w-2/3">
+            <h2 className="text-2xl font-bold text-left">{blogItem.title}</h2>
+            <img className="w-full my-6 rounded-lg" src={blogItem.img_url} alt={blogItem.title} />
             <div className="text-justify" dangerouslySetInnerHTML={{ __html: blogItem.description }} />
+          </div>
+          <div className={'w-1/3 flex flex-col '}>
+            <h3 className={'font-bold'}>Categories</h3>
+            <ul className={'mt-5 leading-10 text-gray-500'}>
+              <li>Technology Trends and News</li>
+              <li>Gaming Insights</li>
+              <li>Security and Privacy</li>
+              <li>Tech Lifestyle and Productivity</li>
+              <li>Product Spotlight</li>
+              <li>How-to Guides and Tutorials</li>
+              <li>Buying Guides and Tips</li>
+            </ul>
           </div>
         </div>
       )}
